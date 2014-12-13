c2cc781eabfdda2666afe8de22d7ee62f775b3b0
leoliam <liamcaleb.asr@gmail.com>
2014-12-13 02:27:16 -0500

    heroku3

--- proyecto/settings/staging.py
+++ proyecto/settings/staging.py
@@ -12,7 +12,7 @@ DATABASES = {
         'USER': 'reaoqihqndirvh',
         'PASSWORD': 'M8uq01hs52NroNpeVT1_KPY2oN',
         'HOST' : 'ec2-54-163-255-191.compute-1.amazonaws.com',
-        'PORT' : '5422',
+        'PORT' : '5432',
     }
 }
 
